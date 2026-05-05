ccbc305966eec3d02648677c8987401d9b5e4889
VirtuousSheep <enderpark000@gmail.com>
2026-05-04 23:31:34 -0400

    flip direction x-axis

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,19 +4,34 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
-    private Vector2 movement;
+    private Rigidbody2D rb;
+    private int facingDirection = 1; // 1 for right, -1 for left
 
-    void Update()
+    private void Awake()
     {
-        movement = Vector2.zero;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        Vector2 movement = Vector2.zero;
         if (Keyboard.current.wKey.isPressed) movement.y += 1;
         if (Keyboard.current.sKey.isPressed) movement.y -= 1;
         if (Keyboard.current.aKey.isPressed) movement.x -= 1;
         if (Keyboard.current.dKey.isPressed) movement.x += 1;
+
+        if (movement.x > 0 && facingDirection < 0 || movement.x < 0 && facingDirection > 0)
+        {
+            flip();
+        }
+
+
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.fixedDeltaTime);
     }
 
-    void FixedUpdate()
+    void flip()
     {
-        transform.Translate(new Vector3(movement.x, 0, movement.y) * speed * Time.deltaTime);
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        //transform.Rotate(0f, 180f, 0f);
     }
 }
