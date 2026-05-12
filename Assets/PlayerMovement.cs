13b71d72180feb31003288c9cf1e5c19aa02d205
VirtuousSheep <enderpark000@gmail.com>
2026-05-11 20:03:25 -0400

    jumping script and other sprites

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,35 +3,142 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
+    public PlayerInput playerInput;
     private Rigidbody2D rb;
+
+    // inputs
+    private bool jumpPressed;
+    private bool jumpReleased;
+
+    [Header("Movement variables")]
+    public float speed = 5f;
+    public float jumpForce = 10f;
+    public Vector2 moveInput;
     private int facingDirection = 1; // 1 for right, -1 for left
 
-    private void Awake()
+    // following makes gravity smoother for jump and fall
+    public float normalGravity;
+    public float fallGravity;
+    public float jumpGravity;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    private bool isGrounded;
+    public float jumpCutMultiplier = 0.5f; // cut jump early or max height when jump button is released
+    
+
+    private void Start()
+    {
+        rb.gravityScale = normalGravity;
+    }
+
+    /*
+     important to use awake instead of start for this, 
+    otherwise the player will not move until the second time you hit play 
+     */
+    void Awake() 
     {
         rb = GetComponent<Rigidbody2D>();
     }
+
+    void Update()
+    {
+        flip();
+        jumpPressed = playerInput.actions["Jump"].IsPressed();
+    }
     void FixedUpdate()
     {
-        Vector2 movement = Vector2.zero;
-        if (Keyboard.current.wKey.isPressed) movement.y += 1;
-        if (Keyboard.current.sKey.isPressed) movement.y -= 1;
-        if (Keyboard.current.aKey.isPressed) movement.x -= 1;
-        if (Keyboard.current.dKey.isPressed) movement.x += 1;
-
-        if (movement.x > 0 && facingDirection < 0 || movement.x < 0 && facingDirection > 0)
-        {
-            flip();
-        }
-
-
-        rb.MovePosition(rb.position + movement.normalized * speed * Time.fixedDeltaTime);
+        ApplyVariableGravity();
+        checkGrounded();
+        handleMovement();
+        handleJump();
     }
 
+    private void handleMovement()
+    {
+        float moveSpeed = moveInput.x * speed;
+        rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void handleJump()
+    {
+        if (isGrounded && jumpPressed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            jumpPressed = false;
+            jumpReleased = false;
+        }
+        if (jumpReleased)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * jumpCutMultiplier);
+        }
+        jumpReleased = false;
+    }
+
+    void ApplyVariableGravity()
+    {
+        /* 0.1 makes sure that the slightest upward or downward movement is not considered as going up or down, 
+        which would cause the gravity to switch back and forth rapidly (appears to stutter)
+        */
+        if (rb.linearVelocity.y > 0.1f) // going up
+        {
+            rb.gravityScale = jumpGravity;
+        }
+        else if (rb.linearVelocity.y < 0.1f) // going down
+        {
+            rb.gravityScale = fallGravity;
+        }
+        else
+        {
+            rb.gravityScale = normalGravity;
+        }
+    }
+
+    void checkGrounded()
+    {
+        // parameters: position of the circle, radius of the circle, layer to check against
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer); 
+    }
     void flip()
     {
-        facingDirection *= -1;
-        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-        //transform.Rotate(0f, 180f, 0f);
+        if (moveInput.x > 0.1f)
+        {
+            facingDirection = 1;
+
+        }
+        else if (moveInput.x < -0.1f){
+            facingDirection = -1;
+        }
+        transform.localScale = new Vector3(facingDirection, 1, 1);
     }
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+    
+    public void OnJump(InputValue value)
+    {
+       if (value.isPressed)
+       {
+            jumpPressed = true;
+            jumpReleased = false;
+       }
+       else
+       {
+            jumpPressed = false;
+            jumpReleased = true;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
+    }
+
 }
